13ec32a50452362f544b15b73afb908470f04859
straw-cell <sayankarv6@gmail.com>
2024-09-19 21:31:20 +0530

    phase2

--- index.js
+++ index.js
@@ -54,4 +54,56 @@ function fontsize(){
    part.style.fontSize=size
 }
 
+var upper=document.getElementsBy("uppercase")
+
+
+function uppercase(){
+    if(part.style.textTransform=="uppercase"){
+        part.style.textTransform="lowercase";
+        document.getElementById("uppercase").style.background="white";
+        document.getElementById("uppercase").style.color="black";
+ 
+    }
+    else{
+        part.style.textTransform="uppercase";
+        document.getElementById("uppercase").style.background=" #0056e0";
+        document.getElementById("uppercase").style.color="white";
+
+
+    }
+
+    
+}
+function pick(){
+    var color=document.getElementById("pick").value;
+    console.log(color);
+    part.style.color=color
+}
+
+function rightalign(){
+    if(part.style.textAlign=="right"){
+       part.style.textAlign="left";
+       document.getElementById("right").style.background="white";
+       document.getElementById("right").style.color="black";
+    }
+    else{
+        part.style.textAlign="right";
+        document.getElementById("right").style.background=" #0056e0";
+        document.getElementById("right").style.color="white";
+    }  
+}
+
+function centeralign(){
+    if(part.style.textAlign=="center"){
+       part.style.textAlign="left";
+       document.getElementById("center").style.background="white";
+       document.getElementById("center").style.color="black";
+    }
+    else{
+        part.style.textAlign="center";
+        document.getElementById("center").style.background=" #0056e0";
+        document.getElementById("center").style.color="white";
+    }  
+}
+
 
